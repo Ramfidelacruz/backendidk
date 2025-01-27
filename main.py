5d42851f88bff8eaec688647f690d56cc08b6b0d
JohnStudio22 <john.goyo@studio22.com.do>
2025-01-27 15:59:52 -0400

    users quite /

--- main.py
+++ main.py
@@ -96,7 +96,7 @@ def get_db():
     finally:
         db.close()
 
-@app.post("/users/", response_model=schemas.User)
+@app.post("/users", response_model=schemas.User)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db_user = db.query(models.User).filter(models.User.email == user.email).first()
     if db_user:
